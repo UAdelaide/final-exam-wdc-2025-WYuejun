a9c2aeceff7f9f00941cf80b92a43e2fa1ec2e9b
WYuejun <a1806362@adelaide.edu.au>
2025-06-20 10:29:37 +0000

    [automatic commit] 2025-06-20 10:29:37 UTC

--- part2/app.js
+++ part2/app.js
@@ -16,7 +16,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     secure: false, // Set to true in production with HTTPS
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    maxAge: 24 * 60 * 60 * 1000 //  Session expires after 24 hours
   }
 }));
 
