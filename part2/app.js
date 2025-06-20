671b165823098c9f8581a97737eb76a642736674
WYuejun <a1806362@adelaide.edu.au>
2025-06-20 06:46:15 +0000

    [automatic commit] 2025-06-20 06:46:15 UTC

--- part2/app.js
+++ part2/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Session configuration
+app.use(session({
+  secret: 'dogwalking-secret-key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false, // Set to true in production with HTTPS
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  }
+}));
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
